495f2e18d22dea67bf6cfb83f4fe48604e4b607b
Michel Hardenberg <MHardenberg@proton.me>
2024-06-21 15:32:19 +0200

    typo

--- src/arrayList.hpp
+++ src/arrayList.hpp
@@ -7,11 +7,11 @@
 template<typename T>
 struct ArrayList {
     T *data;
-    size_t lenght;
+    size_t length;
     size_t capacity;
 
     ArrayList(size_t capacity = 10) {
-        this->lenght = 0;
+        this->length = 0;
         this->capacity = capacity;
         data = (T*) malloc(this->capacity * sizeof(T));
     }
@@ -25,7 +25,7 @@ struct ArrayList {
 
             if (!newData) {return 1;}
 
-            memcpy(newData, data, sizeof(T) * this->lenght);
+            memcpy(newData, data, sizeof(T) * this->length);
             free(data);
             this->capacity += growBy;
             this->data = newData;
@@ -34,19 +34,19 @@ struct ArrayList {
         }
 
     int push(T x) {
-        if (this->lenght >= this->capacity) {
-            if(this->grow(1 + sqrt(this->lenght))) {
+        if (this->length >= this->capacity) {
+            if(this->grow(1 + sqrt(this->length))) {
                 return 1;
             };
         }
 
-        this->data[this->lenght] = x;
-        this->lenght++;
+        this->data[this->length] = x;
+        this->length++;
         return 0;
     }
 
     T* operator[](size_t i) {
-       if (i < lenght) {
+       if (i < length) {
             return this->data + i;
        } 
        return NULL;
--- test/test_alist.cpp
+++ test/test_alist.cpp
@@ -8,14 +8,14 @@ int main (int argc, char *argv[]) {
     ArrayList<char> list(10);
 
     LOG("Capacity:", list.capacity);
-    LOG("Length:", list.lenght);
+    LOG("Length:", list.length);
 
     for (size_t i = 0; i < 20; ++i) {
         list.push(chars[i]);
 
 
         LOG("Capacity:", list.capacity);
-        LOG("Length:", list.lenght);
+        LOG("Length:", list.length);
         LOG("*list[" << i <<"]:", *list[i]);
         LOG("", "");
     }
