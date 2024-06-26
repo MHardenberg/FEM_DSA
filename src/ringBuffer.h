2d8d3feb4de437654de5bd124d5fe83cbee42cdc
Michel Hardenberg <MHardenberg@proton.me>
2024-06-26 13:50:26 +0200

    ringBuffer

--- src/ringBuffer.h
+++ src/ringBuffer.h
@@ -1,62 +1,122 @@
-#include <cstddef>
-#include <cstdlib>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 struct RingBuffer {
  void *data;
- size_t capacity, elementSize, length;
- void *head_ptr, *tail_ptr;
-
+ size_t capacity, elementSize, length,
+        head_idx, tail_idx;
 };
 
-struct RingBuffer createRingBuffer(size_t capacity, size_t elementSize) {
+
+struct RingBuffer *createRINGBUFFER(size_t capacity, size_t elementSize) {
     if (capacity < 3) {
         fprintf(stderr, "%s\n", "Buffer capacity too small.");
         capacity = 10;
     }
-    
+
     void *data = malloc(capacity * elementSize);
-    // convert to ptr of byte sized and increment by elementSize.
-    void *head_ptr = (char *)data + elementSize * capacity/3;
+    if (!data) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    
+    struct RingBuffer *rb = (struct RingBuffer *) malloc(sizeof(struct RingBuffer));
+    if (!rb) {
+        free(data);
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
 
-    struct RingBuffer rb = {
-        .data = data,
-        .capacity = capacity,
-        .elementSize = elementSize,
-        .length = 0,
-        .head_ptr = head_ptr,
-        .tail_ptr = head_ptr,
-    };
-
+    rb->data = data;
+    rb->capacity = capacity;
+    rb->elementSize = elementSize;
+    rb->length = 0;
+    rb->head_idx = 0;
+    rb->tail_idx = 0;
 
     return rb;
 }
 
 
-void freeRingBuffer(struct RingBuffer *rb) {
-    free(rb->data);
-    free(rb);
+void free_RINGBUFFER(struct RingBuffer *rb) {
+    if (rb) {
+        free(rb->data);
+        free(rb);
+    }
 }
 
 
-int resizeRingBuffer(struct RingBuffer *rb, size_t growBy) {
+int resize_RINGBUFFER(struct RingBuffer *rb, size_t growBy) {
     size_t newCapacity = rb->capacity + growBy;
     void *newData = malloc(newCapacity * rb->elementSize);
-    void *newHead = (char *)newData + newCapacity/3 * rb->elementSize;
     if (!newData) {
-        fprintf(stderr, "%s\n", "Allocation Error in resizeRingBuffer");
+        fprintf(stderr, "%s\n", "Allocation Error in resizeRINGBUFFER");
         return 1;
     }
 
-    memcpy(newHead, rb->head_ptr, rb->length * rb->elementSize);
-    free(rb->data);
 
+    for (size_t i = 0; i < rb->length; i++) {
+        size_t idx = (rb->head_idx + i) % rb->capacity;
+        memcpy((char *)newData + i * rb->elementSize,
+                (char *)rb->data + idx * rb->elementSize,
+                rb->elementSize);
+    }
+    free(rb->data);
     rb->data = newData;
-    rb->head_ptr = newHead;
-    rb->tail_ptr = (char *)newHead + rb->length*rb->elementSize;
     rb->capacity = newCapacity;
+    rb->head_idx = 0;
+    rb->tail_idx = rb->length;
+
     return 0;
 }
+
+
+int RINGBUFFER_add(struct RingBuffer *rb, void *elem) {
+    if (!elem) {return 1;}
+    if (rb->length == rb->capacity) {
+        if (resize_RINGBUFFER(rb, rb->length)) {return 1;}   
+    }
+
+    memcpy(
+        (char *)rb->data + rb->tail_idx * rb->elementSize,
+        elem,
+        rb->elementSize
+        );
+
+    rb->tail_idx = (rb->tail_idx + 1) % rb->capacity;
+    rb->length++;
+    return 0;
+}
+
+
+void *RINGBUFFER_peek(struct RingBuffer *rb) {
+    if (rb->length == 0) {return NULL;}
+    void *elem = (char *)rb->data + (rb->head_idx * rb->elementSize);
+    return elem;
+}
+
+
+void *RINGBUFFER_pop(struct RingBuffer *rb, void *elemBuffer) {
+    if (rb->length == 0) {return NULL;}
+
+    if (!elemBuffer) {
+        fprintf(stderr, "No element buffer provied\n");
+        return NULL;
+    }
+
+    memcpy(
+        elemBuffer,
+        (char *)rb->data + rb->head_idx * rb->elementSize,
+        rb->elementSize
+        );
+
+    rb->head_idx = (rb->head_idx + 1) % rb->capacity;
+    rb->length--;
+    return elemBuffer;
+}
+
+
+
 
--- test/test_ringBuffer.c
+++ test/test_ringBuffer.c
@@ -3,9 +3,26 @@
 
 #define LOG(fmt, x) (fprintf(stderr, fmt, x))
 
-
+char chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 int main (int argc, char *argv[]) {
-    LOG("%s\n", "Hello!");
+    struct RingBuffer *rb = createRINGBUFFER(10, sizeof(char));
+    for (size_t i = 0; i < 14; ++i) {
+        RINGBUFFER_add(rb, &chars[i]);
+        LOG("length: %li\t", rb->length);
+        LOG("cap: %li\n", rb->capacity);
+    }
+    LOG("%s\n", "-------------------------");
+
+
+    char *elem = malloc(sizeof(char));
+    for (size_t i = 0; i < 14; ++i) {
+        RINGBUFFER_pop(rb, elem);
+        LOG("elem: %c\t", *elem);
+        LOG("length: %li\t", rb->length);
+        LOG("cap: %li\n", rb->capacity);
+    }
+    free(elem);
+    free_RINGBUFFER(rb);
     return 0;
 }
