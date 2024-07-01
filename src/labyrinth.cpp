97ce85a69945c009e622789cae20d2c7fb760ac3
Michel Hardenberg <MHardenberg@proton.me>
2024-07-01 15:05:33 +0200

    up

--- src/labyrinth.cpp
+++ src/labyrinth.cpp
@@ -2,29 +2,37 @@
 #include <cstring>
 
 
-Labyrinth::Labyrinth(const char * tiles, size_t rows, size_t cols) {
+Labyrinth::Labyrinth(const char * tiles, size_t rows, size_t cols,
+            char startChar, char endChar, char wallChar) {
+        this->startChar = startChar;
+        this->endChar = endChar;
+        this->wallChar = wallChar;
+
         this->rows = rows;
         this->cols = cols;
 
         this->tiles = (char *)malloc(sizeof(char) * rows * cols + 1);
         strncpy(this->tiles, tiles, rows * cols);
-
-        this->visited = (bool *)malloc(sizeof(char) * rows * cols);
-        memset(this->visited, 0, sizeof(bool) * rows * cols);
         this->tiles[rows * cols] = '\0';
-    }
+
+        this->visited = (bool *)malloc(sizeof(bool) * rows * cols);
+        memset(this->visited, 0, sizeof(bool) * rows * cols);
+}
+
 
 Labyrinth::~Labyrinth() {
     free(this->tiles);
     free(this->visited);
 }
 
+
 char &Labyrinth::operator()(size_t r, size_t c) {
     size_t pos = (r * this->cols) + c;
     return this->tiles[pos];
 }
 
-bool &Labyrinth::markVisited(size_t r, size_t c) {
+
+bool &Labyrinth::checkVisited(size_t r, size_t c) {
     size_t pos = (r * this->cols) + c;
     return this->visited[pos];
 }
--- src/labyrinth.hpp
+++ src/labyrinth.hpp
@@ -11,17 +11,35 @@ struct Labyrinth {
     size_t rows, cols;
     char *tiles;
     bool *visited;
+    char startChar, endChar, wallChar;
 
-    Labyrinth(const char * tiles, size_t rows, size_t cols);
+    Labyrinth(const char * tiles, size_t rows, size_t cols,
+            char startChar = 'S', char endChar = 'E', char wallChar = 'W');
 
     ~Labyrinth(); 
 
     char &operator()(size_t r, size_t c);
 
-    bool &markVisited(size_t r, size_t c);
+    bool &checkVisited(size_t r, size_t c);
 };
 
-bool walk();
+bool walk(Labyrinth *lab, size_t row, size_t col) {
+    // if outside
+    if (row >= lab->rows or col >= lab->cols) {
+        return false;
+    }
+
+    // if visited
+    if (lab->checkVisited(row, col)) {
+        return false;
+    }
+    
+    // if wall
+    
+    // if exit
+
+
+};
 
 void solver();
 
