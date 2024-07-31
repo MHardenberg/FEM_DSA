31f17c41e333bccdaa261b4266d69212d4c70c3b
Michel Hardenberg <MHardenberg@proton.me>
2024-07-31 12:49:32 +0200

    qs done

--- src/quick.hpp
+++ src/quick.hpp
@@ -1,52 +1,43 @@
 #include <cstddef>
-#include <iostream>
-#define LOG(x)(std::cout << x)
-#define LOGLN(x)(std::cout << x << std::endl)
-
+//#define SWAP(x, y) x = y ^ x; y = x ^ y; x = y ^ x
+#define SWAP(x, y) {auto temp = x; x = y; y = temp;}
 
 
 template<typename T>
-size_t partition(T* array, size_t lo, size_t hi) {
+static size_t partition(T* array, size_t lo, size_t hi) {
     const T pivot = array[hi];
     size_t idx = lo;
 
     for (size_t i = lo; i < hi; ++i) {
         if (array[i] <= pivot) {
-            array[idx] = array[i] ^ array[idx];
-            array[i] = array[idx] ^ array[i];
-            array[idx] = array[i] ^ array[idx];
-
+            SWAP(array[idx], array[i]);
             ++idx;
         }
     }
 
-    array[hi] = array[idx];
-    array[idx] = pivot;
-
-
+    SWAP(array[idx], array[hi]);
     return idx;
 }
 
 
 template<typename T>
-int quickSort(T* array, size_t length) {
-    
-    
-    size_t pivot = length/2,
-           low = 0,
-           high = length - 1;
-
-
-
-
-
-
-
-
-
-    for (size_t i = 0; i < length; ++i) {
-        LOG(array[i] << " ");
+static void qs(T* array, size_t lo, size_t hi) {
+    if (lo >= hi) {
+        return;
     }
-    LOG(std::endl);
-    return 0;
+
+    const size_t pivotIdx = partition(array, lo, hi);
+    if (pivotIdx > lo) {
+        qs(array, lo, pivotIdx - 1);
+    }
+    qs(array, pivotIdx + 1, hi);
+}
+
+
+template<typename T>
+void quickSort(T* array, size_t length) {
+    size_t low = 0,
+           high = length - 1;
+    
+    qs(array, low, high);
 }
--- test/test_quick.cpp
+++ test/test_quick.cpp
@@ -1,8 +1,16 @@
+#include <iostream>
 #include "../src/quick.hpp"
+
+#define LOG(x) std::cout << x
+#define LOGLN(x) std::cout << x << std::endl
+#define LOGARRAY(array, length) for (size_t i = 0; i < length; ++i)\
+{LOG(array[i]); LOG(", ");} LOG(std::endl)
 
 
 int main (int argc, char *argv[]) {
     int arr[12] = {3, 4, 1, 2, 6, 8, 1, 0, 11, 15, 5, 7};
+    LOGARRAY(arr, 12);
     quickSort(arr, 12);
+    LOGARRAY(arr, 12);
     return 0;
 }
