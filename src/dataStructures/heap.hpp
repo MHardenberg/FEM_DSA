74f6c6ddbd36c20dfb0222a0681c973f53129abb
Michel Hardenberg <MHardenberg@proton.me>
2024-08-09 10:30:36 +0200

    heap

--- src/dataStructures/heap.hpp
+++ src/dataStructures/heap.hpp
@@ -6,30 +6,106 @@
 
 template<typename T>
 struct MinHeap {
- std::vector<T> data;
- size_t length() {
-    return this->data.size();
- };
+std::vector<T> data;
+size_t length;
 
- void insert(T value);
+void insert(T value) {
+    this->data.push_back(value);
+    this->heapifyUp(this->length);
+    this->length++;
+}
 
- private:
-     inline size_t parent(size_t idx) {
+void pop(T& dest) {
+    if (this->length == 0) {
+        return;
+    }
+
+    dest = this->data[0];
+    if (this->length == 1) {
+        this->data.pop_back();
+        this->length--;
+        return;
+    }
+
+    this->data[0] = this->data[this->length - 1];
+    this->length--;
+    this->heapifyDown(0);
+    this->data.pop_back();
+}
+
+private:
+     inline size_t parent(const size_t idx) const {
             return (idx - 1)/2;
      }
-     inline size_t childLeft(size_t idx) {
+     inline size_t childLeft(const size_t idx) const {
             return 2 * idx + 1;
      }
-     inline size_t childRight(size_t idx) {
+     inline size_t childRight(const size_t idx) const {
             return 2 * idx + 2;
      }
 
-     void heapifyUp() {
+     void heapifyUp(const size_t currentIdx) {
+         if (currentIdx == 0) {
+             return;
+         }
 
+         const T currentVal = this->data[currentIdx];
+
+         const size_t parentIdx = this->parent(currentIdx);
+         const T parentVal = this->data[parentIdx];
+
+         if (parentVal > currentVal) {
+            // swap node values
+            this->data[currentIdx] = parentVal;
+            this->data[parentIdx] = currentVal;
+
+            heapifyUp(parentIdx);
+         }
      }
 
-     void heapifyDown() {
+     void heapifyDown(const size_t currentIdx) {
+         if (currentIdx >= this->length) {
+            return;
+         }
 
+         const size_t lIdx = this->childLeft(currentIdx);
+         if (lIdx >= this->length) {
+             return;
+             // out of children!
+         }
+         const size_t rIdx = this->childRight(currentIdx);
+         const T currentVal = this->data[currentIdx];
+
+
+
+         T minValue;
+         size_t minIdx;
+
+         // check if right child exists
+         //
+         if (rIdx >= this->length) {
+             minValue = this->data[lIdx];
+             minIdx = lIdx;
+         }
+         // if left child is the lesser 
+         else if (
+                 this->data[lIdx] <= this->data[rIdx]
+                 ){
+             minValue = this->data[lIdx];
+             minIdx = lIdx;
+         }
+         // if right child is the lesser
+         else {
+             minValue = this->data[rIdx];
+             minIdx = rIdx;
+
+         }
+            
+         if (minValue < currentVal) {
+             this->data[currentIdx] = minValue;
+             this->data[minIdx] = currentVal;
+            this->heapifyDown(minIdx);
+         }
      }
 };
 
--- test/test_heap.cpp
+++ test/test_heap.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include "../src/dataStructures/heap.hpp"
-#define LOG(desc, x) (std::cout << desc << " " << x << std::endl)
+#define LOG(x) (std::cout << x)
+#define LOGLN(x) (std::cout << x << std::endl)
 
 
 
 int main (int argc, char *argv[]) {
-    LOG("Test", '\n');
+
+    MinHeap<int> heap;
+    for (size_t i = 15; i > 0; i--) {
+        heap.insert(i);
+    }
+    
+    for (auto x: heap.data) {
+        LOG(x);
+        LOGLN("");
+    }
+
+    LOGLN("------------------");
+    int dest;
+    while(heap.length) {
+        for (auto x: heap.data) {
+            LOG(x);
+            LOGLN("");
+        }
+
+        heap.pop(dest);
+        LOG("Popped: ");
+        LOGLN(dest);
+    }
+
     return 0;
 }
